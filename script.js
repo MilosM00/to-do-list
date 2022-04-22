c4b3d4bbecc779863d795965774a3f404cdf2721
Milos Milijasevic <milosrm20005@gmail.com>
2022-04-22 12:13:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+
+const enterItem = document.querySelector(`.input-item`);
+const buttonAddNewItem = document.querySelector(`.button-add`);
+const displayNewItem = document.querySelector(`.display-new-item`);
+
+
