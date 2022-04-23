5de77020512bc89c82acc53460b401208aebb7b1
Milos Milijasevic <milosrm20005@gmail.com>
2022-04-23 10:30:51 +0200

    js updated

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ const buttonAddNewItem = document.querySelector(`.button-add`);
 const displayNewItem = document.querySelector(`.display-new-item`);
 
 
+buttonAddNewItem.addEventListener(`click`, () =>{
+    addNewItem();
+});
+
+enterItem.addEventListener(`keyup`, (e) =>{
+    if(e.keyCode === 13){
+        e.preventDefault();
+        addNewItem();
+    }
+});
+
+const addNewItem = function(){
+    if(enterItem.value.length === 0) return enterItem.focus();
+
+    let html = `
+    <div class="display-look">
+        <div class="look">
+            <p class="item-info">${enterItem.value}</p>
+            <button class="button-x" onclick="this.parentNode.parentNode.remove()">×</button>
+        </div>
+    </div>
+    `;
+
+    displayNewItem.insertAdjacentHTML(`beforeend`, html);
+
+    enterItem.value = ``;
+    enterItem.focus();  
+};
